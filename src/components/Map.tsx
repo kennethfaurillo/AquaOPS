d4b56fe0856fcbfada800f90e97cb9815934ecc7
Kenneth Faurillo <kenneth.edison.faurillo@eee.upd.edu.ph>
2024-07-12 14:24:11 +0800

    change theme selector to toggle

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,21 +1,17 @@
 import ResetViewControl from '@20tab/react-leaflet-resetview';
 import axios from 'axios';
 import { DivIcon, Icon } from 'leaflet';
-import { BadgeCheckIcon, BadgeHelpIcon, BadgeMinusIcon, CalendarRangeIcon, LucideIcon, MoonIcon, SunIcon } from 'lucide-react';
+import { BadgeCheckIcon, BadgeHelpIcon, BadgeMinusIcon, LucideIcon, MoonIcon, SunIcon } from 'lucide-react';
+import moment from 'moment';
 import { useCallback, useEffect, useState } from 'react';
 import { GeoJSON, MapContainer, Marker, TileLayer, Tooltip } from 'react-leaflet';
 import { toast } from 'sonner';
 import { pipelines } from '../assets/pipelines';
+import { useDrawerDialogContext } from '../hooks/useDrawerDialogContext';
 import './Map.css';
 import { DataLog, Datalogger } from './Types';
 import { Button } from './ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from './ui/command';
-import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
-import { Separator } from './ui/separator';
-import { useDrawerDialogContext } from '../hooks/useDrawerDialogContext';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
-import moment from 'moment';
 
 let loggerIcon = new Icon({
   iconUrl: "src/assets/meter.png",
@@ -60,7 +56,6 @@ function LoggerMapCard() {
   const [map, setMap] = useState(null)
   const [weight, setWeight] = useState(5); // Initial weight
   const [basemap, setBasemap] = useState(basemaps.at(0))
-  // const [mapTheme, setMapTheme] = useState("light")
 
   const { setLogger, setChartDrawerOpen, } = useDrawerDialogContext()
 
@@ -70,7 +65,6 @@ function LoggerMapCard() {
       const zoom = map.getZoom();
       // Adjust weight based on zoom level
       const newWeight = Math.max(2, 1 + (zoom - 13) / 1); // Example calculation
-      // console.log(newWeight)
       setWeight(newWeight);
     };
 
@@ -89,9 +83,6 @@ function LoggerMapCard() {
       try {
         const loggersInfoResponse = await axios.get(`http://${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT}/api/logger/`)
         const latestLogsResponse = await axios.get(`http://${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT}/api/latest_log/`)
-        // setLoggersInfo(loggersInfoResponse.data)
-        // setLatestLogs(latestLogsResponse.data)
-        //@ts-ignore
         let tempLoggersLatest = new Map()
         loggersInfoResponse.data.map((logger: Datalogger) => {
           latestLogsResponse.data.map((log: DataLog) => {
@@ -104,65 +95,11 @@ function LoggerMapCard() {
         setLoggersLatest(tempLoggersLatest)
       }
       catch (error) {
-        //@ts-ignore
         console.log(error)
       }
-      // finally {
-      //   setLoading(false)
-      // }
     }
     fetchData()
   }, [])
-
-  const DisplaySelectors = ({ map }) => {
-    const [openPopover, setOpenPopover] = useState(false)
-    return (
-      <>
-        <Popover open={openPopover} onOpenChange={setOpenPopover}>
-          <PopoverTrigger asChild>
-            <Button
-              variant="outline"
-              size="default"
-              className=" justify-start text-white border-white/20 bg-blue-800/50"
-            >
-              {basemap ? (
-                <>
-                  <basemap.icon className="mr-2 h-4 w-4 shrink-0" />
-                  {basemap.label}
-                </>
-              ) : (
-                <>+ Set Basemap</>
-              )}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="p-0" side="right" align="start">
-            <Command>
-              {/* <CommandInput placeholder="Change basemap..." /> */}
-              <CommandList>
-                <CommandEmpty>No results found.</CommandEmpty>
-                <CommandGroup>
-                  {basemaps.map((bmap) => (
-                    <CommandItem
-                      key={bmap.name}
-                      value={bmap.name}
-                      className={`${bmap.name === basemap?.name ? "bg-piwad-yellow-100" : "bg-none"} gap-2`}
-                      onSelect={(name) => {
-                        setBasemap(basemaps.find((priority) => priority.name === name) || null)
-                        setOpenPopover(false)
-                      }}
-                    >
-                      <bmap.icon />
-                      <span>{bmap.label}</span>
-                    </CommandItem>
-                  ))}
-                </CommandGroup>
-              </CommandList>
-            </Command>
-          </PopoverContent>
-        </Popover>
-      </>
-    )
-  }
 
   const DisplayPosition = ({ map }) => {
     const center = [51.505, -0.09]
@@ -188,25 +125,17 @@ function LoggerMapCard() {
         <div>
           Coordinates: {position.lat.toFixed('6')}°, {position.lng.toFixed('6')}°
         </div>
-        <div className='mb-3 sm:-mb-6 sm:mt-2 sm:space-x-4'>
-          {DisplaySelectors(map)}
-        </div>
+        <div className='mb-3 sm:-mb-6 sm:mt-2 sm:space-x-4'/>
       </>
     )
   }
 
   const onEachPipeline = (feature, layer) => {
-    // console.log(feature.properties)
     if (feature.properties && feature.properties.ogr_fid) {
-      // layer.bindTooltip(feature.properties.ogr_fid)
-      // layer.bindTooltip(<Tooltip permanent>{feature.properties.ogr_fid}</Tooltip>)
-      // layer.on('')
       layer.on('click', () => {
         console.log("click")
         console.log(feature.properties)
-        // setHoverPipeline(feature.properties)
         toast.info(`Pipeline #${feature.properties?.ogr_fid} ${feature.properties?.location.toUpperCase()}`, {
-          // description: `Size: ${feature.properties.size} | Length ${feature.properties.lenght}`,
           description: <>
             <span>Size: {feature?.properties.size}</span>
             <span> | Length: {feature?.properties.lenght.replace('.', '')}</span>
@@ -216,36 +145,31 @@ function LoggerMapCard() {
             </div>
           </>,
         })
-        // layer.openTooltip();
       });
-      // layer.on('mouseover', () => {
-      //   console.log("hover")
-      //   setHoverPipeline(feature.properties)
-      //   // layer.openTooltip();
-      // });
-      // layer.on('mouseout', () => {
-      //   console.log("unhover")
-      //   setHoverPipeline(null)
-      //   // layer.closePopup();
-      // });
     }
   };
 
-  // const displayMap = useMemo(() => (
+  const themeToggleOnclick = () => {
+      setBasemap(basemaps.find((bmap) => bmap.name != basemap.name))
+  }
   const displayMap = (() => (
     <MapContainer // @ts-ignore
-      center={[13.58438280013, 123.2738403740]} ref={setMap} style={{ height: '71vh' }}
+      center={[13.58438280013, 123.2738403740]} ref={setMap} style={{ height: '78dvh' }}
       scrollWheelZoom={true} zoom={13.5} maxZoom={18} minZoom={13} doubleClickZoom={false}
       maxBounds={[[13.676173, 123.111745], [13.516072, 123.456730]]}>
       <ResetViewControl title="Reset View" icon={"🔍"} />
       <TileLayer
         url={basemap ? basemap.url : "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"}
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        className='z-0'
-
       />
+      {basemap ?
+        basemap.name == "osmLight" ?
+          <Button className='absolute bottom-8 right-4 z-[400] size-12 p-0 rounded-full opacity-80' onClick={themeToggleOnclick}><MoonIcon /></Button>
+          : <Button className='absolute bottom-8 right-4 z-[400] size-12 p-0 rounded-full opacity-80' variant={"secondary"} onClick={themeToggleOnclick}><SunIcon /></Button>
+        : null
+      }
       <GeoJSON data={pipelines} style={(feature) => ({
-        color: basemap?.name === "stdDark" ? colorMap[feature?.properties.size] : "#58D68D90",//"#6792A0",
+        color: basemap?.name === "stdDark" ? colorMap[feature?.properties.size] : "#6792A090",//"#6792A090",
         weight: weight,
       })}
         onEachFeature={onEachPipeline}
@@ -269,15 +193,14 @@ function LoggerMapCard() {
                     {loggerData.CurrentFlow ? <>{loggerData.CurrentFlow}<em> lps</em></> : null}
                   </div>
                   <div className='text-slate-600 font-light text-[.55rem] drop-shadow-xl text-right'>
-                    {loggerData.LogTime ? <>{moment(loggerData.LogTime.replace('Z',''), true).format('M/D HH:mm')}<br /></> : null}
+                    {loggerData.LogTime ? <>{moment(loggerData.LogTime.replace('Z', ''), true).format('M/D HH:mm')}<br /></> : null}
                   </div>
                 </Tooltip>
               </Marker>
               <Marker position={[loggerData.Latitude, loggerData.Longitude]} icon={new DivIcon({ iconSize: [0, 0] })}>
                 {basemap?.name == "stdDark" ?
-                  // TODO: fix text color not changing
-                  <Tooltip permanent direction='bottom' className={"logger-label-dark"}>{loggerData.Name.replaceAll('-', ' ').split('_').slice(2)}</Tooltip> :
-                  <Tooltip permanent direction='bottom' > {loggerData.Name.replaceAll('-', ' ').split('_').slice(2)}</Tooltip>
+                  <div><Tooltip permanent direction='bottom' className='logger-label-dark' >{loggerData.Name.replaceAll('-', ' ').split('_').slice(2)}</Tooltip></div> :
+                  <Tooltip permanent direction='bottom'>{loggerData.Name.replaceAll('-', ' ').split('_').slice(2)}</Tooltip>
                 }
               </Marker>
             </div>
@@ -290,24 +213,24 @@ function LoggerMapCard() {
 
   return (
     <>
-      <Card className='col-span-full xl:col-span-9 z-0 drop-shadow-xl' >
+      <Card className='col-span-full xl:col-span-9 z-0 drop-shadow-xl rounded-b-lg overflow-hidden'>
         <CardHeader className='rounded-t-lg bg-piwad-lightblue-600'>
           <CardTitle className='text-slate-950'>
             <div className="grid grid-cols-6">
-              <div className="col-span-6 md:col-span-2">
+              <div className="col-span-6 sm:col-span-2">
                 <p className="mb-1 text-piwad-lightyellow-300">Data Logger Map</p>
-                <Separator className='mt-2 w-11/12' />
-                <div className="text-base text-slate-200 mb-2">{map ? <DisplayPosition map={map} /> : null}</div>
+                {/* <Separator className='mt-2 w-[1/12]'/> */}
+                <div className="text-base text-slate-200 mb-2 ">{map ? <DisplayPosition map={map} /> : null}</div>
               </div>
-              <div className="col-span-6  lg:col-span-4 flex items-center space-x-4 rounded-md border-2 border-piwad-yellow-0 -my-2 py-2">
+              <div className="col-span-6 sm:col-span-4 flex items-center space-x-4 rounded-md border-2 border-piwad-yellow-0 -my-2 py-2">
                 <div className="grid grid-cols-9 flex-1 space-y-1 mx-4">
-                  <div className="text-piwad-yellow-50 hidden sm:flex text-sm md:text-xl font-medium co leading-none col-span-full justify-center sm:justify-normal sm:col-span-2 flex items-center">
+                  <div className="text-piwad-yellow-50 hidden sm:flex text-sm md:text-xl font-medium co leading-none col-span-full justify-center sm:justify-normal sm:col-span-2 items-center">
                     Logger Status:</div>
-                  <div className="text-white text-xs md:text-xl font-medium leading-none col-span-3 justify-center sm:justify-normal sm:col-span-2 flex items-center">
+                  <div className="text-white text-xs lg:text-xl font-medium leading-none col-span-3 justify-center md:justify-normal sm:col-span-2 flex items-center">
                     {5}&nbsp;<BadgeCheckIcon color='lightgreen' />&nbsp;Active</div>
-                  <div className="text-white text-xs md:text-xl font-medium leading-none col-span-3 justify-center sm:justify-normal sm:col-span-2 flex items-center">
+                  <div className="text-white text-xs lg:text-xl font-medium leading-none col-span-3 justify-center md:justify-normal sm:col-span-2 flex items-center">
                     {0}&nbsp;<BadgeMinusIcon color='red' />&nbsp;Disabled</div>
-                  <div className="text-white text-xs md:text-xl font-medium leading-none col-span-3 justify-center sm:justify-normal sm:col-span-2 flex items-center">
+                  <div className="text-white text-xs lg:text-xl font-medium leading-none col-span-3 justify-center md:justify-normal sm:col-span-2 flex items-center">
                     {2}&nbsp;<BadgeHelpIcon color='yellow' />&nbsp;Unknown</div>
                 </div>
               </div>
@@ -320,7 +243,6 @@ function LoggerMapCard() {
           {displayMap()}
         </CardContent>
       </Card>
-
     </>
   )
 }
